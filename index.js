0dcb35e56122da593f0ef1e0c1bda568a4c02a03
whyriod <whyriod@byu.edu>
2022-12-05 12:32:06 -0700

    big push

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 /*******************************************************************************
 * File: index.js
-* Summary: Basic backend server for our project. 
+* Summary: Basic backend server for our project.
 *  ~ Holiness to the Lord ~
 *******************************************************************************/
 
 // Table variable for the db table name
-db_table = "donuts"
+let db_table = "iphone"
 let express = require("express");
 let cors = require('cors')
 let app = new express();
 app.use(cors())
+// app.use(function (req, res, next) {
+// res.setHeader('Access-Control-Allow-Origin', '*');
+// res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+// res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+// res.setHeader('Access-Control-Allow-Credentials', true);
+// next();
+// });
+
 
 // Html for home pages:
 
@@ -17,37 +25,34 @@ app.use(cors())
 const knex = require("knex")({
     client: "mysql",
     connection: {
-        host: "",       //Fill in
-        user: "",       //Fill in
-        password: "",   //Fill in
-        database:"",    //Fill in
+        host: "awseb-e-qpbfgbanrb-stack-awsebrdsdatabase-fsnxyzbtdgci.cgfad7fi6x3f.us-east-2.rds.amazonaws.com",
+        user: "admin",       //Fill in
+        password: "admin123",   //Fill in
+        database:"project3Finaldb",    //Fill in
         port: 3306,
     },
 });
 
 // Basic home page. If for some reason they hit this route, on 3000,
-// It will redirect them to the home page. 
+// It will redirect them to the home page.
 app.get("/",(req,res) => {
-    res.sendFile("index.html");
+    res.sendFile("./index.html",{root: '.'});
 });
- 
-//If they hit /donuts, then they will have JSON returned to them. Of course, 
-//In the future this should be our app data once we have that set up. 
+
+//If they hit /donuts, then they will have JSON returned to them. Of course,
+//In the future this should be our app data once we have that set up.
 app.get("/" + db_table,(req,res) => {
-    res.send('{"items": [{"1":{"picture":"img.png","name":"Bob"}}]}');
-
-    // Database stuff for when we make it. 
-    // knex.select()
-    // .from(db_table)
-    // .then((result) => {
-
-    //     // This will show in the console what we got. 
-    //     console.log(result);
-    //     res.send(result);
-    // }).catch(error => {
-    //     console.log(error);
-    //     res.send("Internal Server Error");
-    // });
+    //Stuff
+    //res.send('{"items": [{"1":{"picture":"img.png","name":"Bob"}}]}');
+    knex.select().from(db_table)
+    .then((result) => {
+    // This will show in the console what we got.
+    console.log(result);
+    res.send(result);
+    }).catch(error => {
+    console.log(error);
+    res.send("Internal Server Error");
+    });
 });
 
 app.listen(3000);
